c61b4bd9ab4f26e91030cbb19202af0882af221d
M.Taimoor Hassan <imtaimoor218@gmail.com>
2023-03-02 17:26:59 +0500

    Give a cornerRadius
    I Gave a cornerRadius to Btn5

--- Calculator(UI Respensive)/ViewController.swift
+++ Calculator(UI Respensive)/ViewController.swift
@@ -9,8 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btn5: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        btn5.layer.cornerRadius = 50
         // Do any additional setup after loading the view.
     }
 
