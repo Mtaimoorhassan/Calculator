91cb3d30c5791ac4ca183a2d6d31b4f6b95a535e
M.Taimoor Hassan <mtaimoor.tbox@gmail.com>
2023-03-02 19:05:26 +0500

    Calculator
    Please Check UI of Caluculator

--- Calculator(UI Respensive)/ViewController.swift
+++ Calculator(UI Respensive)/ViewController.swift
@@ -9,11 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var btn5: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        btn5.layer.cornerRadius = 50
         // Do any additional setup after loading the view.
     }
 
